9286cb1f0e916d4595717940056d5fc228d34117
Alek2750 <alek2750@gmail.com>
2019-03-13 20:48:22 +0100

    updatePerson metode

--- src/main/java/dataFacade/PersonFacade.java
+++ src/main/java/dataFacade/PersonFacade.java
@@ -10,12 +10,12 @@ import javax.persistence.Persistence;
  * @author Alek
  */
 public class PersonFacade {
-    
+
     EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu2", null);
     EntityManager em = emf.createEntityManager();
-    
-    public Person addPerson(Person p){
-        try{
+
+    public Person addPerson(Person p) {
+        try {
             em.getTransaction().begin();
             em.persist(p);
             em.getTransaction().commit();
@@ -24,9 +24,9 @@ public class PersonFacade {
             em.close();
         }
     }
-    
-    public Person deletePerson(int id){
-        try{
+
+    public Person deletePerson(int id) {
+        try {
             em.getTransaction().begin();
             Person p = em.find(Person.class, id);
             em.remove(p);
@@ -36,10 +36,25 @@ public class PersonFacade {
             em.close();
         }
     }
-    
-    public Person updatePerson(Person p){
+
+    public Person updatePerson(Person p) {
         //Skal nok bruge DTO her
+
+        try {
+            em.getTransaction().begin();
+            Person pers = em.find(Person.class, p.getId());
+
+            if (pers != null) {
+                pers = p;
+                em.merge(pers);
+                em.getTransaction().commit();
+                return pers;
+            }
+
+        } finally {
+            em.close();
+        }
         return null;
     }
-    
+
 }
